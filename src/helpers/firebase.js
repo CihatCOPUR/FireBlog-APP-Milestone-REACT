d986fd3fb8831c840fc03f497dbca4b8a23c868b
Cihat <copurcihat@gmail.com>
2022-11-19 19:05:37 +0300

    gg

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import {getAuth , GoogleAuthProvider} from "firebase/auth";
+import {getFirestore} from  "firebase/firestore"
 
 
 // Your web app's Firebase configuration
@@ -15,6 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+export const database = getFirestore(app);
 
 export const auth = getAuth(app);
 
